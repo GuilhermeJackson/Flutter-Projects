7992706e731d8a219cc92ee88d685fbf469a948c
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-02-22 14:33:56 -0300

    Ordenando tarefas concluidas para baixo da lista e não concluidas para cima da lista ao dar um 'Refresh' na propria lista

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -17,6 +17,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _toDoList = []; //criando uma lista
+
+  Map<String, dynamic> _lastRemoved;  //lista de itens removidos
+  int _lastRemovePos;
+
   final toDoController = TextEditingController();
 
   //ctrl . O = seleciona initState - metodo q carrega as informações ao inicializar o app
@@ -63,7 +67,7 @@ class _HomeState extends State<Home> {
                 Expanded(   // Usado para o TextField e RaisedButton ficarem alinhados
 
                   child: TextField(
-                  controller: toDoController,   //passado as informações para o controlador
+                    controller: toDoController,   //passado as informações para o controlador
                     decoration: InputDecoration(
                         labelText: "Nova tarefa",
                         labelStyle: TextStyle(color: Colors.blue)
@@ -80,31 +84,90 @@ class _HomeState extends State<Home> {
             ),
           ),
 
+
           Expanded(
 
-            child: ListView.builder(
+            child: RefreshIndicator(onRefresh: _refresh,
+              child: ListView.builder(    // ListView é a lista e builder permite criar a lista conforme for construindo a mesma (estilo recyclerView)
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,      //pega a quantidade de item na lista
                 // ignore: missing_return
-                itemBuilder: (context, index){    //index é a posição do elemento da lista q esta sendo desenhando
-                  return CheckboxListTile(        // é a celula da lista q ira exibir as informações setadas
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],  // se o checkBox está checado ou não
-                    secondary: CircleAvatar(
-                      child: Icon(_toDoList[index]["ok"] ? Icons.check: Icons.face),),//Cria um ícone de acerto
-                    onChanged: (seTaChecado){ //chama funcao quando o status do checkBox muda passando como paramentro bool o seTaChecado
-                      setState(() {   //atualiza a tela
-                        _toDoList[index]["ok"] = seTaChecado;   //se seTaChecado = true, selecionando, clica de novo no checkBox e seTaChecado fica falso
-                        _saveData();  // salva o arquivo quando o status é altarado para ok ou true :)
-                      });
-                    },
-                  );
-                }), // ListView é a lista e builder permite criar a lista conforme for construindo a mesma (estilo recyclerView)
+                itemBuilder: _buildItem),
+            )
           ),
         ],
       ),
     );
   }
+
+  //Atualização demora apenas 1 segundo
+  Future<Null>_refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+//Ordenando por tarefas nao realizadas na parte de cima
+      _toDoList.sort((a, b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
+  //funcao para retornar
+  Widget _buildItem(context, index){    //index é a posição do elemento da lista q esta sendo desenhando
+    return Dismissible(   //Dismissible - permite q arrastemos o item para direita para fazer alguma ação
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()), //para chave nao se repetir, pega o tempo atual em mili-segundos
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9 ,0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.horizontal,   //Faz o direcionamento de arrastar da esquerda para a direta
+      child: CheckboxListTile(        // é a celula da lista q ira exibir as informações setadas
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],  // se o checkBox está checado ou não
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check: Icons.face),),//Cria um ícone de acerto
+        onChanged: (seTaChecado){ //chama funcao quando o status do checkBox muda passando como paramentro bool o seTaChecado
+          setState(() {   //atualiza a tela
+            _toDoList[index]["ok"] = seTaChecado;   //se seTaChecado = true, selecionando, clica de novo no checkBox e seTaChecado fica falso
+            _saveData();  // salva o arquivo quando o status é altarado para ok ou true :)
+          });
+        },
+      ),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);    //adiciona o item na lista de itens excluidos pegando a posição
+          _lastRemovePos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(label: "Desfazer", onPressed: (){
+              setState(() {
+                _toDoList.insert(_lastRemovePos, _lastRemoved);
+                _saveData();
+              });
+            }),
+            duration: Duration(seconds: 3),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+
+        });
+
+      },  // Ação ao arrastar para direita
+    );
+  }
+  /**/
+
 
   //Usado para pegar o caminho do diretorio do dispositivo
   Future<File> _getFile() async {
@@ -121,7 +184,7 @@ class _HomeState extends State<Home> {
     String data = json.encode(
         _toDoList); //Trasformando a list em um json e armazenando na String 'data'
     final file =
-        await _getFile(); //Espera o _getFile retorna o diretorio para salvat do constante file
+    await _getFile(); //Espera o _getFile retorna o diretorio para salvat do constante file
     return file.writeAsString(
         data); // pega o arquivo e com a lista de tarefas e escreve como texto
   }
@@ -131,7 +194,7 @@ class _HomeState extends State<Home> {
     //async é usado quando não conseguimos ter os dados instantaniamente
     try {
       final file =
-          await _getFile(); //Espera o _getFile retorna o diretorio para salvat do constante file
+      await _getFile(); //Espera o _getFile retorna o diretorio para salvat do constante file
       return file.readAsString(); //Le o arquivo para poder retorna
     } catch (e) {
       return null;
