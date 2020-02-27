f918c10159a7acb13bbb04c9c616952f90155dce
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-02-27 16:17:05 -0300

    Usando lendo as informações do FireBase. Sempre que o BD for alterado, é chamada uma função

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
 
-  runApp(MyApp(
+  runApp(MyApp());
+  // Esperando os dados para inserir no snapshot
+  //DocumentSnapshot snapshot = await Firestore.instance.collection("menssagens").document("msg1").get();
 
-  ));
+  // Snapshot recebe uma foto exato do momento do banco
+  //QuerySnapshot snapshot = await Firestore.instance.collection("menssagens").getDocuments();
+
+  // Sempre que houver uma mudança, uma função é chamada
+  Firestore.instance.collection("menssagens").snapshots().listen((snapshot){
+    for (DocumentSnapshot doc in snapshot.documents) {
+      print(doc.data);
+    }
+  });
+
 }
+
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Firestore.instance.collection("menssagens").document().setData({"from": "Daniel", "texto":"Olá"});
+    // TODO: implement build
     return Container();
   }
 }
