4ad5822185295e24dfaab4c1fc76c9a25a8eee19
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-02-27 01:00:44 -0300

    Passando informações para o FireBase via comandos

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -3,14 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
 
-  Firestore.instance.collection("teste").document("teste").setData({"teste":"teste"});
+  runApp(MyApp(
 
-  runApp(MyApp());
-
+  ));
 }
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    Firestore.instance.collection("menssagens").document().setData({"from": "Daniel", "texto":"Olá"});
     return Container();
   }
 }
