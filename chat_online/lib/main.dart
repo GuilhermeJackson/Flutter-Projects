d834960ce961ac72c62a5676bfdea64a766266da
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-02-27 17:48:13 -0300

    Definindo temas caso a plataforma seja iOS ou Android

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -18,12 +18,59 @@ void main() async {
   });
 
 }
+//Definindo a cor padrão de varios componentes
+final ThemeData KIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  //Tonalidade
+  primaryColor: Colors.grey[300],
+  //Brilho
+  primaryColorBrightness: Brightness.light
+);
+//Definindo a cor padrão de varios componentes
+final ThemeData KDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return MaterialApp(
+      title: "Nem a NASA rastreia...",
+      // Remover a faixa de debug da tela do app
+      debugShowCheckedModeBanner: false,
+       // Define um tema se for iphone e outro tema se for android
+       theme: Theme.of(context).platform == TargetPlatform.iOS ? KIOSTheme : KDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // SafeArea - ignora as barrinhas do iPhone e android (barra com botao de voltar e minimizar os app e tal)
+    return SafeArea(
+        bottom: false,
+        top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          
+        ),
+      ),
+    );
+
   }
 }
