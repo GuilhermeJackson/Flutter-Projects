5aef5bf1d37569f51c713fe1af8d6679ec273543
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-02-27 18:16:25 -0300

    W/P 27/02/2020 - Criando o layout

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -17,22 +17,24 @@ void main() async {
     }
   });
 
+
+
 }
-//Definindo a cor padrão de varios componentes
-final ThemeData KIOSTheme = ThemeData(
-  primarySwatch: Colors.orange,
-  //Tonalidade
-  primaryColor: Colors.grey[300],
-  //Brilho
-  primaryColorBrightness: Brightness.light
-);
-//Definindo a cor padrão de varios componentes
-final ThemeData KDefaultTheme = ThemeData(
-  primarySwatch: Colors.purple,
-  accentColor: Colors.orangeAccent[400],
-);
 
 class MyApp extends StatelessWidget {
+//Definindo a cor padrão de varios componentes
+  final ThemeData KIOSTheme = ThemeData(
+      primarySwatch: Colors.orange,
+      //Tonalidade
+      primaryColor: Colors.grey[300],
+      //Brilho
+      primaryColorBrightness: Brightness.light
+  );
+//Definindo a cor padrão de varios componentes
+  final ThemeData KDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple,
+    accentColor: Colors.orangeAccent[400],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -67,10 +69,55 @@ class _ChatScreenState extends State<ChatScreen> {
           elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: Column(
-          
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(
+
+              ),
+            ),
+          ],
         ),
       ),
     );
 
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        // Inserir o const no na margin, deixa o app mais leve
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform  == TargetPlatform.iOS ? BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey[200]))
+        ) : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: (){},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration.collapsed(hintText: "Escreva sua mensagem..."),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
