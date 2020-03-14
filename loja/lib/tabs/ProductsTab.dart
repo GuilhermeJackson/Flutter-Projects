4a92f375dd0f1e448564260114aeb4577564be7e
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-03-14 10:56:30 -0300

    W/P 14/03/2020

--- loja/lib/tabs/ProductsTab.dart
+++ loja/lib/tabs/ProductsTab.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
@@ -12,10 +15,16 @@ class ProductsTab extends StatelessWidget {
         if (!snapshot.hasData)
           return Center(child: CircularProgressIndicator(),);
         else {
-          return ListView(
-            children: <Widget>[
+          // Pega todo o documento, transforma em uma CategoryTile, e transforma em uma lista
+          var dividedTiles = ListTile.divideTiles(
+              tiles: snapshot.data.documents.map((doc) {
+                return CategoryTile(doc);
+              }).toList(),
+              color: Colors.grey[500])
+              .toList();
 
-            ],
+          return ListView(
+            children: dividedTiles,
           );
         }
       },
---
+++ loja/lib/tiles/category_tile.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja/screens/category_screen.dart';
+
+class CategoryTile extends StatelessWidget {
+  // Pega as categorias
+  final DocumentSnapshot snapshot;
+  // Deve ser passado um documento snapshot
+  CategoryTile(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      // leading = icone localizado na esquerda da celula da ListView
+      leading: CircleAvatar(
+        radius: 25.0,
+        backgroundColor: Colors.transparent,
+        // pegando a imagem do banco de dados
+        backgroundImage: NetworkImage(snapshot.data["icon"]),
+      ),
+      title: Text(snapshot.data["title"], style: TextStyle(color: Theme.of(context).primaryColor),),
+      // trailing = setinha localizado a direita do titulo da categoria
+      trailing: Icon(Icons.keyboard_arrow_right),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>CategoryScreen(snapshot))
+        );
+      },
+    );
+  }
+}
