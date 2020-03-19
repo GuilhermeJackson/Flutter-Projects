59b98a6271788d3a7516aeda9b1e8f48123305a2
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-03-18 22:12:53 -0300

    Tela do produto - exibindo array de imagens, nome e preço

--- loja/lib/screens/product_screen.dart
+++ loja/lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja/datas/product_data.dart';
 
@@ -14,6 +15,61 @@ class _ProductScreenState extends State<ProductScreen> {
   _ProductScreenState(this.product);
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          // AspectRatio = Inserindo tamanho da lista
+          AspectRatio(
+            aspectRatio: 0.9,
+            // Pegando um arrei de imagens do firebase > product
+            child: Carousel(
+              images: product.images.map((url){
+                return NetworkImage(url);
+              }).toList(),
+              // dotSize = é a boloinha abaixo da imagem q identifica qual imagem do array vc esta
+              dotSize: 4.0,
+              // dotSpacing = espaço entre as bolinhas
+              dotSpacing: 15.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              // autoplay = faz a imagem passar automaticamente se true
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              // CrossAxisAlignment.stretch = tenta ocupar o maximo de espaço na vertical
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(product.title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500
+                  ),
+                  // maxLines = quantidade maxima de linhas
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
