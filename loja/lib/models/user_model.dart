661a3961cfb6e31b2649c30cabaee2379cd23302
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-03-21 16:36:47 -0300

    Ccódigo para criar usuraio

--- loja/lib/models/user_model.dart
+++ loja/lib/models/user_model.dart
@@ -1,22 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 // Model = objeto q guarda o estado de alguma coisa (no caso o login)
 class UserModel extends Model {
-  //Usuario
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  FirebaseUser firebaseUser;
+  //Carrega as informações do usuário
+  Map<String, dynamic> userData = Map();
 
   bool isLoading = false;
 
   // deslogar
-  void signUp(){
+  // @required = usando para auto completar automatico quando a função for chamada (quando usado, o mesmo é obrigatório)
+  void signUp({@required Map<String, dynamic> userData, @required String pass, @required VoidCallback onSucess, @required VoidCallback  onFail}) {
+    // notificando o usuario que esta sendo carregado
     isLoading = true;
     notifyListeners();
 
+    //tentativa de criar o usuario
+    _auth.createUserWithEmailAndPassword(
+        email: userData["email"],
+        password: pass
+    ).then((user) async{
+          firebaseUser = user as FirebaseUser;
 
+          await _saveUserData(userData);
+
+          onSucess();
+          isLoading = false;
+          notifyListeners();
+    }).catchError((e){
+        onFail();
+        isLoading = false;
+        notifyListeners();
+    });
   }
 
   //fazer o login
-  void signIn(){
+  void signIn() async{
+    isLoading = true;
+    notifyListeners();
 
+    await Future.delayed(Duration(seconds: 3));
+
+    isLoading = false;
+
+    notifyListeners();
   }
 
   //recuperar senha
@@ -27,5 +59,10 @@ class UserModel extends Model {
   //verifica se o usuario está logado
   bool isLoggedIn (){
 
+  }
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async{
+    this.userData = userData;
+    await Firestore.instance.collection("users").document(firebaseUser.uid).setData(userData);
   }
 }
