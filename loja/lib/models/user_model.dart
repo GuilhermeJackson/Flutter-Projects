b7ed8075b49628da04bc54c8c372a035ee15e335
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-03-29 22:18:15 -0300

    Realizando o login e salvando o mesmo para próxima vez que entrar no app

--- loja/lib/models/user_model.dart
+++ loja/lib/models/user_model.dart
@@ -6,14 +6,19 @@ import 'package:scoped_model/scoped_model.dart';
 // Model = objeto q guarda o estado de alguma coisa (no caso o login)
 class UserModel extends Model {
 
-  FirebaseAuth auth = FirebaseAuth.instance;
-  Firestore _db = Firestore.instance;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
   FirebaseUser firebaseUser;
   //Carrega as informações do usuário
   Map<String, dynamic> userData = Map();
 
   bool isLoading = false;
 
+  //Carrega usuário que já esatava logado quando o app é aberto novamente
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(_loadCurrentUser);
+  }
 
   // @required = usando para auto completar automatico quando a função for chamada (quando usado, o mesmo é obrigatório)
   void signUp({@required Map<String, dynamic> userData, @required String pass, @required VoidCallback onSucess, @required VoidCallback  onFail}) {
@@ -22,12 +27,13 @@ class UserModel extends Model {
     notifyListeners();
 
     //tentativa de criar o usuario
-    auth.createUserWithEmailAndPassword(
+    _auth.createUserWithEmailAndPassword(
         email: userData["email"],
         password: pass
     ).then((user) async{
 
       firebaseUser = user;
+
 
       await _saveUserData(userData);
 
@@ -42,19 +48,28 @@ class UserModel extends Model {
   }
 
   //fazer o login
-  void signIn({@required String email, @required String pass,
-    @required VoidCallback onSuccess, @required VoidCallback onFail}) async {
+  void signIn({@required String email, @required String pass, @required VoidCallback onSuccess, @required VoidCallback onFail}) async {
     isLoading = true;
     notifyListeners();
 
-    await Future.delayed(Duration(seconds: 3));
+    _auth.signInWithEmailAndPassword(email: email, password: pass).then(
+            (user) async {
+              firebaseUser = user;
 
-    isLoading = true;
-    notifyListeners();
+              await _loadCurrentUser();
+
+              onSuccess();
+              isLoading = false;
+              notifyListeners();
+    }).catchError((e){
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
   void signOut() async{
-    await auth.signOut();
+    await _auth.signOut();
 
     userData = Map();
     firebaseUser = null;
@@ -72,9 +87,25 @@ class UserModel extends Model {
     return firebaseUser != null;
   }
 
+  // Registra as informações da conta do usuario no banco de dados
   Future<Null> _saveUserData(Map<String, dynamic> userData) async{
     this.userData = userData;
     await Firestore.instance.collection("users").document(firebaseUser.uid).setData(userData);
+  }
+
+  // função para pegar as informações do usuario q está logado
+  Future<Null> _loadCurrentUser() async{
+    if(firebaseUser == null)
+      firebaseUser = await _auth.currentUser();
+    if(firebaseUser != null) {
+      if (userData["name"] == null) {
+        DocumentSnapshot docUser = await Firestore.instance.collection("users")
+            .document(firebaseUser.uid)
+            .get();
+        userData = docUser.data;
+      }
+    }
+    notifyListeners();
   }
 
 }
--- loja/lib/screens/login_screen.dart
+++ loja/lib/screens/login_screen.dart
@@ -4,100 +4,134 @@ import 'package:loja/models/user_model.dart';
 import 'package:loja/screens/sigup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class LoginScreen extends StatelessWidget {
+// ao tentar digitar, o mesmo tenta mudar a view e como o stateless ele nao consegue mudar a view entao deve ser transforamando em statefull
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Entrar"),
-        centerTitle: true,
-        // botao para criar conta ao lado do title
-        actions: <Widget>[
-          FlatButton(
+      key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("Entrar"),
+          centerTitle: true,
+          // botao para criar conta ao lado do title
+          actions: <Widget>[
+            FlatButton(
 
-            child: Text(
+              child: Text(
                 "Criar Conta",
                 style: TextStyle(fontSize: 15.0
                 ),
+              ),
+              textColor: Colors.white,
+              onPressed: (){
+                // pushReplacement = quando clicado, a tela é substituida (ao criar conta, o usuario nao precisa fazer o login, ela ja vai estar logado)
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context)=>SingUpScreen())
+                );
+              },
             ),
-            textColor: Colors.white,
-            onPressed: (){
-              // pushReplacement = quando clicado, a tela é substituida (ao criar conta, o usuario nao precisa fazer o login, ela ja vai estar logado)
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context)=>SingUpScreen())
-              );
-            },
-          ),
-        ],
-      ),
-      // Form = ele valida os campos de um formulário
-      body: ScopedModelDescendant<UserModel>(
-        builder: (context, child, model) {
-          // se o model esta carregando alguma coisa, CircularProgressIndicator é chamado
-          if(model.isLoading)
-            return Center(child: CircularProgressIndicator());
+          ],
+        ),
+        // Form = ele valida os campos de um formulário
+        body: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model) {
+            // se o model esta carregando alguma coisa, CircularProgressIndicator é chamado
+            if(model.isLoading)
+              return Center(child: CircularProgressIndicator());
 
-          return Form(
-            key: _formKey,
-            child: ListView(
-              padding: EdgeInsets.all(16.0),
-              children: <Widget>[
-                //TextFormField = Apenas um campo de texto
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "E-mail"
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  // validator = realiza uma verificação de e-mail
-                  // ignore: missing_return
-                  validator: (text) {
-                    if(text.isEmpty || !text.contains("@")) return "E-mail inválido";
-                  },
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Senha"
-                  ),
-                  obscureText: true,
-                  // ignore: missing_return
-                  validator: (text){
-                    if(text.isEmpty || text.length <= 6) return "Senha inválida!";
-                  },
-                ),
-                // mover algum widget para direita da tela
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: FlatButton(
-                    onPressed: (){},
-                    child: Text("Esqueci minha senha", textAlign: TextAlign.right),
-                    padding: EdgeInsets.zero,
-                  ),
-                ),
-                SizedBox(height: 16.0,),
-                SizedBox(
-                  height: 44.0,
-                  child: RaisedButton(
-                    child: Text("Entrar",
-                      style: TextStyle(fontSize: 20.0),
+            return Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                children: <Widget>[
+                  //TextFormField = Apenas um campo de texto
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        hintText: "E-mail"
                     ),
-                    textColor: Colors.white,
-                    color: Theme.of(context).primaryColor,
-                    onPressed: (){
-                      // pedi para validar os campos quando o botao é pressionando
-                      if(_formKey.currentState.validate()){
-
-                      }
-                      model.signIn();
+                    keyboardType: TextInputType.emailAddress,
+                    // validator = realiza uma verificação de e-mail
+                    // ignore: missing_return
+                    validator: (text) {
+                      if(text.isEmpty || !text.contains("@")) return "E-mail inválido";
                     },
                   ),
-                ),
-              ],
-            ),
-          );
-        },
-      )
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    controller: _passController,
+                    decoration: InputDecoration(
+                        hintText: "Senha"
+                    ),
+                    obscureText: true,
+                    // ignore: missing_return
+                    validator: (text){
+                      if(text.isEmpty || text.length <= 6) return "Senha inválida!";
+                    },
+                  ),
+                  // mover algum widget para direita da tela
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: (){},
+                      child: Text("Esqueci minha senha", textAlign: TextAlign.right),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                  SizedBox(height: 16.0,),
+                  SizedBox(
+                    height: 44.0,
+                    child: RaisedButton(
+                      child: Text("Entrar",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      textColor: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      onPressed: (){
+                        // pedi para validar os campos quando o botao é pressionando
+                        if(_formKey.currentState.validate()){
+
+                        }
+                        model.signIn(
+                            email:_emailController.text,
+                            pass:  _passController.text,
+                            onSuccess: _onSuccess,
+                            onFail: _onFail
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        )
+    );
+  }
+  void _onSuccess(){
+    Navigator.of(context).pop();
+  }
+
+  void _onFail(){
+    _scaffoldKey.currentState.showSnackBar(
+        SnackBar(content: Text("Falaha ao realizar o login!!!"),
+            backgroundColor: Colors.redAccent,
+            duration: Duration(seconds: 3)
+        )
     );
   }
 }
+
