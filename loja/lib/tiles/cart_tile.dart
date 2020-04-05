cdc420a5f51ae647f752f64ad0515141d6be82ee
GuilhermeJackson <guilherme.lamim96@gmail.com>
2020-04-05 20:41:35 -0300

    Exibindo os produtos do Firebase no carrinho

--- loja/lib/tiles/cart_tile.dart
+++ loja/lib/tiles/cart_tile.dart
@@ -12,14 +12,77 @@ class CartTile extends StatelessWidget {
   Widget build(BuildContext context) {
 
     Widget _buildContent(){
-      
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120.0,
+            child: Image.network(
+                cartProduct.productData.images[0],
+                fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(cartProduct.productData.title,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 17.0),
+                  ),
+                  Text("${cartProduct.size}",
+                  style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  Text("R\$${cartProduct.productData.price.toStringAsFixed(2)}",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                   children: <Widget>[
+                     IconButton(
+                       icon: Icon(Icons.remove),
+                       color: Theme.of(context).primaryColor,
+                       onPressed:cartProduct.quantity < 1 ? (){
+
+                       } : null,
+                     ),
+                     Text(cartProduct.quantity.toString()),
+                     IconButton(
+                       icon: Icon(Icons.add),
+                       color: Theme.of(context).primaryColor,
+                       onPressed: (){
+
+                       },
+                     ),
+                     FlatButton(
+                       child: Text("Remover"),
+                       textColor: Colors.grey[500],
+                       onPressed: (){
+
+                       },
+                     )
+                   ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      );
     }
 
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
       child: cartProduct.productData == null ?
         FutureBuilder<DocumentSnapshot>(
-          future: Firestore.instance.collection("product").document(cartProduct.category).collection("Items").document(cartProduct.pid).get(),
+          future: Firestore.instance.collection("product").document(cartProduct.category).collection("items").document(cartProduct.pid).get(),
           // ignore: missing_return
           builder: (context, snapshot){
             if(snapshot.hasData){
@@ -35,7 +98,7 @@ class CartTile extends StatelessWidget {
             }
           },
         ) :
-      _buildContent();
+      _buildContent()
     );
   }
 }
